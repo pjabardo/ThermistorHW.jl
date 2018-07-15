0944ec6678995784e694412e827503173db21f63
Paulo Jabardo <pjabardo@gmail.com>
2018-07-15 17:00:25 -0300

    Implementei o controlador via software.
    No software está functionando. Vamos ver se eu consigo fazer isso em
    hardware! Não está documentado. Vamos ver o que rola...

--- notebooks/cta-pulsed.jl
+++ notebooks/cta-pulsed.jl
@@ -21,8 +21,8 @@ struct PulsedCTA
     Vi::Float64
     "Frequência do PWM"
     freq::Float64
-    PulsedCTA(Rt::Thermistor, Ri=200.0, Rf=10e3, Cf=10e-6, Vi=24.0,
-              freq=490.0, duty=0.5) = new(Rt, Ri, Rf, Cf, Vi, freq)
+    PulsedCTA(Rt::Thermistor, Ri=100.0, Rf=10e3, Cf=47e-6, Vi=24.0,
+              freq=1000.0, duty=0.5) = new(Rt, Ri, Rf, Cf, Vi, freq)
 end
 
 """
@@ -49,6 +49,8 @@ end
 Principais parâmetros de transferência de calor.
 """
 struct HeatTrans
+    "Temperatura de operação do termistor"
+    Tw::Float64
     "Diâmetro do termistor"
     D::Float64
     "Temperatura ambiente"
@@ -57,7 +59,21 @@ struct HeatTrans
     Pa::Float64
     "Área superficial externa do termistos"
     A::Float64
-    HeatTrans(Dmm, Ta=20.0, Pa=101.325) = new(Dmm/1000, Ta, Pa, 4π*(Dmm/2000)^2)
+    "Propriedades de transferência de calor do termistor"
+    S::Solid
+    m::Float64
+    mcp::Float64
+    function HeatTrans(Tw=85.0, Dmm=2.0,  Ta=20.0, Pa=93.0, S=Fe₂O₃)
+        D = Dmm/1000
+        R = D/2
+        A = 4π*R^2
+        Vol = A*R/3
+        m = Vol * S.ρ
+        mcp = m * S.cₚ
+        
+        new(Tw, D, Ta, Pa, A, S, m, mcp)
+    end
+    
 end
 
 """
@@ -117,4 +133,77 @@ function pulsedcta(dy, y, p, t)
 end
 
     
+abstract type AbstractVelocity end
+
+
+struct VelConst <: AbstractVelocity
+    v::Float64
+    VelConst(v=10.0) = new(v)
 end
+
+
+velocity(v::VelConst, t=0.0) = v.v
+(v::VelConst)(t=0.0) = velocity(v,t)
+
+struct VelFreq <: AbstractVelocity
+    vmean::Float64
+    freq::Float64
+    ampl::Float64
+    VelFreq(vmean=10.0, freq=0.5, ampl=0.5) = new(vmean, freq, ampl)
+end
+velocity(v::VelFreq, t=0.0) = v.vmean + v.ampl * sin(2π*v.freq*t)
+(v::VelFreq)(t=0.0) = velocity(v,t)
+    
+struct VelRamp <: AbstractVelocity
+    α::Float64
+    VelRamp(α=1.0) = new(α)
+end
+
+velocity(v::VelRamp, t=0.0) = v.α * t
+(v::VelRamp)(t=0.0) = velocity(v,t)
+
+struct VelRampConst <: AbstractVelocity
+    t0::Float64
+    V0::Float64
+    VelRampConst(V0=10.0, t0=10.0) = new(V0, t0)
+end
+velocity(v::VelRampConst, t=0.0) = (t < v.t0) ? t*v.V0/v.t0 : v.V0
+(v::VelRampConst)(t=0.0) = velocity(v, t)
+    
+struct VelStep <: AbstractVelocity
+    t0::Float64
+    V0::Float64
+    V1::Float64
+    VelStep(t0=10.0, V1=10.0, V0=0.0) = new(t0, V0, V1)
+end
+velocity(v::VelStep, t=0.0) = (t < v.t0) ? v.V0 : v.V1
+(v::VelStep)(t=0.0) = velocity(v, t)
+
+struct VelUpDown <: AbstractVelocity
+    t0::Float64
+    t1::Float64
+    t2::Float64
+    V0::Float64
+    VelUpDown(V0=10.0, t0=6.0, t1=12, t2=18.0) = new(t0, t1, t2, V0)
+end
+function velocity(v::VelUpDown, t=0.0)
+    if t < v.t0
+        return v.V0*t/v.t0
+    elseif t >= v.t0 && t < v.t1
+        return v.V0
+    elseif t >= v.t1 && t < v.t2
+        return (1.0 - (t-v.t1)/(v.t2-v.t1)) * v.V0
+    else
+        return 0.0
+    end
+end
+(v::VelUpDown)(t=0.0) = velocity(v, t)
+
+    
+hamcpfun(htrans::HeatTrans, vel::AbstractVelocity, t=0.0, Tw=htrans.Tw) = hafun(htrans, vel(t), Tw)/htrans.mcp
+
+hamcpfun(htrans::HeatTrans, U::Number, t=0.0, Tw=htrans.Tw) = hafun(htrans, U, Tw)/htrans.mcp
+
+
+end
+
