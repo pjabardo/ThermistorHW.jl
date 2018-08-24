09547466f84a4777891ae0d522007a019559b2c9
Paulo José Saiz Jabardo <pjabardo@gmail.com>
2018-08-24 16:28:34 -0300

    Trabalhando no anemômetro CC de 16 canais.
    Fiz um programinha arduino. Acho que tem alguma coisa errada no hardware.
    Quando eu coloco nos canais 2 e 0, o nodemcu lê errado. Mas fiz uns testes fora e funcion.
    
    Amanhã eu dou uma olhada.

---
+++ arduino/nodemcu32s/cca-16/cca-16.ino
@@ -0,0 +1,60 @@
+/*
+  ESP 32 Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+  The ESP32 has an internal blue LED at D2 (GPIO 02)
+*/
+
+const int NSAMPLES = 1154;
+
+const int AICHANS[] = {35, 34, 39, 36, 26, 25, 33, 32, 13, 12, 14, 27, 15, 2, 0, 4};
+const int NCHANS = 16;
+int aivals[NCHANS];
+
+
+void readchans(const int chans[], int nchans, int aival[], int nsamples){
+
+  for (int k = 0; k < nchans; ++k){
+    aival[k] = 0;
+  }
+
+  for (int i = 0; i < nsamples; ++i){
+    for (int k = 0; k < nchans; ++k){
+      aival[k] += analogRead(chans[k]);  
+    }
+  }
+
+  for (int k = 0; k < nchans; ++k){
+    aival[k] /= nsamples;
+  }
+  
+}
+
+void setup() 
+{
+  analogSetAttenuation(ADC_6db)
+  for (int i = 0; i < NCHANS; ++i){
+    //pinMode(AICHANS[i], INPUT);
+     
+  }
+  Serial.begin(9600);
+  
+  
+}
+
+
+void loop() 
+{
+  int t1 = millis();
+  readchans(AICHANS, NCHANS, aivals, NSAMPLES);
+  
+  Serial.printf("IPT\t%d", t1);
+  for (int k = 0; k < NCHANS; ++k){
+    Serial.printf("\t%d", aivals[k]);
+  }
+
+  Serial.printf("\n");
+  
+
+  
+}
+
